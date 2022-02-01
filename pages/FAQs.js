7e0d31a8ee6373c1e92225133551fa72c47163aa
Abdur-Razzak-Rouzex <abdurrazzak1663@gmail.com>
2022-02-02 01:55:52 +0600

    FAQ page in front end is done

--- pages/FAQs.js
+++ pages/FAQs.js
@@ -1,8 +1,12 @@
 import Layout from "../components/Layout";
-import {Box, Container, Grid, Typography} from "@mui/material";
+import {Accordion, AccordionDetails, AccordionSummary, Box, Container, Divider, Grid, Typography} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FAQ from "../models/FAQ";
+import db from "../utils/db";
+import NoDataFoundComponent from "../components/common/NoDataFoundComponent";
 
-
-const FAQs = () => {
+const FAQs = (props) => {
+    const {faqs} = props;
 
     return (
         <Layout>
@@ -13,138 +17,49 @@ const FAQs = () => {
                             Frequent Asked Questions
                         </Typography>
                     </Box>
-                    <Grid container spacing={3}>
-                        {/*<FrequentQuestions
-                            title="Frequest questions"
-                            subtitle="Don't forget, you can contact us on anytime"
-                            items={[
-                                {
-                                    title: 'Lorem ipsum dolor sit amet?',
-                                    content: (
-                                        <Typography component="p">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                            sagittis felis erat, nec vulputate nibh mattis quis.
-                                            Pellentesque habitant morbi tristique senectus et netus et
-                                            malesuada fames ac turpis egestas. Cras pharetra non lectus ut
-                                            bibendum. Etiam suscipit luctus egestas. Nunc vitae placerat
-                                            metus, sed pellentesque risus. Suspendisse potenti. Vestibulum
-                                            volutpat felis eget urna tempus gravida. Nunc dui risus, viverra
-                                            in erat et, fermentum interdum dolor. Ut non lorem ipsum. Donec
-                                            lobortis eu elit et venenatis. Praesent id arcu porta tellus
-                                            feugiat hendrerit. Sed pretium ullamcorper felis, id tincidunt
-                                            nunc suscipit ac.
-                                        </Typography>
-                                    ),
-                                },
-                                {
-                                    title: 'Lorem ipsum dolor sit amet?',
-                                    subtitle: 'This is a subtitle',
-                                    content: (
-                                        <>
-                                            <Typography component="p" paragraph>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                                sagittis felis erat, nec vulputate nibh mattis quis.
-                                                Pellentesque habitant morbi tristique senectus et netus et
-                                                malesuada fames ac turpis egestas. Cras pharetra non lectus ut
-                                                bibendum. Etiam suscipit luctus egestas. Nunc vitae placerat
-                                                metus, sed pellentesque risus. Suspendisse potenti. Vestibulum
-                                                volutpat felis eget urna tempus gravida. Nunc dui risus,
-                                                viverra in erat et, fermentum interdum dolor. Ut non lorem
-                                                ipsum. Donec lobortis eu elit et venenatis. Praesent id arcu
-                                                porta tellus feugiat hendrerit. Sed pretium ullamcorper felis,
-                                                id tincidunt nunc suscipit ac.
+                    {faqs?.length > 0 ? (
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                {faqs?.map(faq => (
+                                    <Accordion key={faq._id} sx={{marginBottom: '10px'}}>
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                        >
+                                            <Typography>{faq?.questions}</Typography>
+                                        </AccordionSummary>
+                                        <Divider />
+                                        <AccordionDetails>
+                                            <Typography>
+                                                {faq?.answer}
                                             </Typography>
-                                            <Typography component="p">
-                                                Ut eleifend lacus in ex interdum feugiat. Phasellus lacinia
-                                                faucibus ex. Integer sit amet accumsan dui. Integer diam
-                                                tortor, auctor vel maximus eu, faucibus quis sem. Curabitur et
-                                                purus et nisi tempor interdum eu at velit. Quisque id placerat
-                                                mi. Interdum et malesuada fames ac ante ipsum primis in
-                                                faucibus. Fusce et suscipit justo, non porttitor enim. Class
-                                                aptent taciti sociosqu ad litora torquent per conubia nostra,
-                                                per inceptos himenaeos.
-                                            </Typography>
-                                        </>
-                                    ),
-                                },
-                                {
-                                    title: 'Lorem ipsum dolor sit amet?',
-                                    content: (
-                                        <Typography component="p">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                            sagittis felis erat, nec vulputate nibh mattis quis.
-                                            Pellentesque habitant morbi tristique senectus et netus et
-                                            malesuada fames ac turpis egestas. Cras pharetra non lectus ut
-                                            bibendum. Etiam suscipit luctus egestas. Nunc vitae placerat
-                                            metus, sed pellentesque risus. Suspendisse potenti. Vestibulum
-                                            volutpat felis eget urna tempus gravida. Nunc dui risus, viverra
-                                            in erat et, fermentum interdum dolor. Ut non lorem ipsum. Donec
-                                            lobortis eu elit et venenatis. Praesent id arcu porta tellus
-                                            feugiat hendrerit. Sed pretium ullamcorper felis, id tincidunt
-                                            nunc suscipit ac.
-                                        </Typography>
-                                    ),
-                                },
-                                {
-                                    title: 'Lorem ipsum dolor sit amet?',
-                                    content: (
-                                        <Typography component="p">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                            sagittis felis erat, nec vulputate nibh mattis quis.
-                                            Pellentesque habitant morbi tristique senectus et netus et
-                                            malesuada fames ac turpis egestas. Cras pharetra non lectus ut
-                                            bibendum. Etiam suscipit luctus egestas. Nunc vitae placerat
-                                            metus, sed pellentesque risus. Suspendisse potenti. Vestibulum
-                                            volutpat felis eget urna tempus gravida. Nunc dui risus, viverra
-                                            in erat et, fermentum interdum dolor. Ut non lorem ipsum. Donec
-                                            lobortis eu elit et venenatis. Praesent id arcu porta tellus
-                                            feugiat hendrerit. Sed pretium ullamcorper felis, id tincidunt
-                                            nunc suscipit ac.
-                                        </Typography>
-                                    ),
-                                },
-                                {
-                                    title: 'Lorem ipsum dolor sit amet?',
-                                    content: (
-                                        <Typography component="p">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                            sagittis felis erat, nec vulputate nibh mattis quis.
-                                            Pellentesque habitant morbi tristique senectus et netus et
-                                            malesuada fames ac turpis egestas. Cras pharetra non lectus ut
-                                            bibendum. Etiam suscipit luctus egestas. Nunc vitae placerat
-                                            metus, sed pellentesque risus. Suspendisse potenti. Vestibulum
-                                            volutpat felis eget urna tempus gravida. Nunc dui risus, viverra
-                                            in erat et, fermentum interdum dolor. Ut non lorem ipsum. Donec
-                                            lobortis eu elit et venenatis. Praesent id arcu porta tellus
-                                            feugiat hendrerit. Sed pretium ullamcorper felis, id tincidunt
-                                            nunc suscipit ac.
-                                        </Typography>
-                                    ),
-                                },
-                            ]}
-                        />*/}
-                        QUESTION and answers will be here
-                    </Grid>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                ))}
+                            </Grid>
+                        </Grid>
+                    ):(
+                        <NoDataFoundComponent />
+                    )}
+
                 </Container>
             </Box>
         </Layout>
     );
 }
 
-/*export async function getServerSideProps() {
+export async function getServerSideProps() {
     await db.connect();
 
-    const whyChooseUsDoc = await WhyChooseUs.find({})
+    const faqDoc = await FAQ.find({})
         .lean()
-        .sort({
-            updatedAt: -1,
-        })
 
     return {
         props: {
-            whyChooseUsData: whyChooseUsDoc.map(db.convertDocToObj)
+            faqs: faqDoc.map(db.convertDocToObj)
         },
     };
-}*/
+}
 
 export default FAQs;
